c4e167296b4e5147e2320d096bc38d06f250cf65
waicode <waicode37@gmail.com>
2021-03-28 22:13:00 +0900

    fix: sync

--- handler.js
+++ handler.js
@@ -2,32 +2,42 @@
 
 const chromium = require("chrome-aws-lambda");
 
-module.exports.hello = async (event) => {
+function isSlsLocal() {
+  if (process.env.IS_LOCAL) {
+    console.info("Running locally");
+  }
+  if (process.env.SLS_STAGE == "local") {
+    console.info("Stage is local");
+  }
+  return process.env.IS_LOCAL || process.env.SLS_STAGE == "local";
+}
+
+module.exports.sync = async (event) => {
   let result = null;
   let browser = null;
 
   try {
-    if (process.env.SLS_STAGE == "local" || process.env.SLS_STAGE == "dev") {
-      browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: null, // local puppeteer in node_modules(dev)
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true,
-      });
-    } else {
-      browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true,
-      });
-    }
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: isSlsLocal() ? null : await chromium.executablePath, // local puppeteer in node_modules(dev)
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    // 1. 変更があったcssまたはjsをイベントから取得
+
+    // 以下、複数件のループ処理
+
+    // 2. 対象ページの存在を確認する
+
+    // 3. 対象ページが存在しない場合は新規作成
+
+    // 4. 存在する場合は一度消してから新規作成
 
     let page = await browser.newPage();
 
-    await page.goto(event.url || "https://www.google.com/");
+    await page.goto(`https://scrapbox.io/${process.env.PROJECT_NAME}/`);
 
     result = await page.title();
   } catch (error) {
