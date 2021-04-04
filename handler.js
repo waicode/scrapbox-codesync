eef9f6c2ffcb6cab39096f67ffe3f10dd5d2b386
waicode <waicode37@gmail.com>
2021-04-04 23:58:33 +0900

    feat: all sync

--- handler.js
+++ handler.js
@@ -17,9 +17,10 @@ const listFiles = (dir) =>
 
 // 行頭にタブを入れる（文頭と\nの次に\tを入れ、文末に\tがあれば除去）
 const addTabHeadOfLine = (fileData) =>
-  fileData.replace(/^/g, /\t/g).replace(/\n/g, /\n\t/g).replace(/\t$/g, /\t$/g);
+  fileData.replace(/^/g, "\t").replace(/\n/g, "\n\t").replace(/\t$/g, "");
 
-function getSidCookieJson() {
+// 認証用Cookie
+const getSidCookieJson = () => {
   return [
     {
       domain: "scrapbox.io",
@@ -35,9 +36,9 @@ function getSidCookieJson() {
       id: 1,
     },
   ];
-}
+};
 
-function isSlsLocal() {
+const isSlsLocal = () => {
   if (process.env.IS_LOCAL) {
     console.info("Running locally");
   }
@@ -45,9 +46,9 @@ function isSlsLocal() {
     console.info("Stage is local");
   }
   return process.env.IS_LOCAL || process.env.SLS_STAGE == "local";
-}
+};
 
-function isSignatureValid(body, headers) {
+const isSignatureValid = (body, headers) => {
   const sigHashAlg = "sha256";
   const sigHeaderName = "X-Hub-Signature-256";
   const sigHeader = headers[sigHeaderName];
@@ -56,9 +57,20 @@ function isSignatureValid(body, headers) {
   hmac.update(body, "utf8");
   const digest = `${sigHashAlg}=` + hmac.digest("hex");
   return digest.length == sigHeader.length && digest == sigHeader;
-}
+};
 
-function okResponse(result) {
+const launchBrowser = async () => {
+  return await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: isSlsLocal() ? null : await chromium.executablePath, // local puppeteer in node_modules(dev)
+    headless: chromium.headless,
+    slowMo: 300,
+    ignoreHTTPSErrors: true,
+  });
+};
+
+const okResponse = (result) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -70,9 +82,9 @@ function okResponse(result) {
       2
     ),
   };
-}
+};
 
-function unauthorizedResponse() {
+const unauthorizedResponse = () => {
   return {
     statusCode: 401,
     body: JSON.stringify(
@@ -83,9 +95,9 @@ function unauthorizedResponse() {
       2
     ),
   };
-}
+};
 
-function fatalResponse() {
+const fatalResponse = () => {
   return {
     statusCode: 500,
     body: JSON.stringify(
@@ -96,6 +108,40 @@ function fatalResponse() {
       2
     ),
   };
+};
+
+async function deletePage(
+  page,
+  targetUrl,
+  editMenuSelector,
+  deleteBtnSelector
+) {
+  await Promise.all([
+    page.waitForSelector(editMenuSelector),
+    page.goto(targetUrl),
+  ]);
+
+  await Promise.all([
+    page.waitForSelector(deleteBtnSelector),
+    page.click(editMenuSelector),
+  ]);
+
+  const deleteBtn = await page.$(deleteBtnSelector);
+  if (deleteBtn) {
+    return await Promise.all([
+      page.waitForNavigation(),
+      page.click(deleteBtnSelector),
+    ]);
+  } else {
+    return Promise.resolve();
+  }
+}
+
+async function addPage(page, targetUrl, editMenuSelector) {
+  return await Promise.all([
+    page.waitForSelector(editMenuSelector),
+    page.goto(targetUrl),
+  ]);
 }
 
 module.exports.sync = async (event) => {
@@ -113,94 +159,86 @@ module.exports.sync = async (event) => {
   let result = null;
   let browser = null;
 
-  console.info(`chromium.headless: ${chromium.headless}`);
-
   try {
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: isSlsLocal() ? null : await chromium.executablePath, // local puppeteer in node_modules(dev)
-      headless: chromium.headless,
-      slowMo: 300,
-      ignoreHTTPSErrors: true,
-    });
+    browser = await launchBrowser();
 
     let page = await browser.newPage();
 
+    // ダイアログのアラートは常に受け入れる
+    page.on("dialog", async (dialog) => {
+      await dialog.accept();
+    });
+
     // scrapbox.ioのクッキーにconnect.sidを設定
-    const sidCookie = getSidCookieJson();
-    await page.setCookie(...sidCookie);
-
-    // 1. 変更があったcssまたはjsをイベントから取得
-
-    // 1. codeフォルダ配下のstyleとscriptのパスを取得
-    const cssFilesList = listFiles("code/css");
+    await page.setCookie(...getSidCookieJson());
 
     // UserCSSページタイトルをフォルダ名から取得
+    const cssFilesList = listFiles("code/css");
     const userCssPageDicList = await Promise.all(
       cssFilesList.map(async (path) => {
-        let fileData = await fs.readFileSync(path, "utf-8");
+        let cssFileData = await fs.readFileSync(path, "utf-8");
         return {
           title: path.match(new RegExp("code/css/(.+)/.+.css"))[1],
-          code: addTabHeadOfLine(fileData),
+          code: addTabHeadOfLine(cssFileData),
         };
       })
     );
-
-    console.log(userCssPageDicList);
 
     // UserScriptページタイトルをフォルダ名から取得
     const jsFilesList = listFiles("code/js");
     const userScriptPageDicList = await Promise.all(
       jsFilesList.map(async (path) => {
-        const jsReg = new RegExp("code/js/(.+)/.+.js");
-        let fileData = await fs.readFileSync(path, "utf-8");
-        // 行頭にタブを入れる（最初と\nの次、ただし最後の\nの次は不要）
-        fileData = fileData
-          .replace(/^/g, "\t")
-          .replace(/\n/g, "\n\t")
-          .replace(/\t$/g, "");
-        console.log(fileData);
-        return { title: path.match(jsReg)[1], code: fileData };
+        let jsFileData = await fs.readFileSync(path, "utf-8");
+        return {
+          title: path.match(new RegExp("code/js/(.+)/.+.js"))[1],
+          code: addTabHeadOfLine(jsFileData),
+        };
       })
     );
     console.log(userScriptPageDicList);
 
-    // 以下、複数件のループ処理
-    const cssTagName = "#UserCSS";
-    for (let userCssPageDic of userCssPageDicList) {
-      let cssPage = `${cssTagName} + \n\n + "code:style.css" + \n + ${userCssPageDic.code} + \n\n`;
-    }
-
-    const scriptTagName = "#UserScript";
-
-    // 2. 対象ページの存在を確認する
-
-    // 3. 対象ページが存在しない場合は新規作成
-
-    // 4. 存在する場合は一度消してから新規作成
-
-    await page.goto(
-      `https://scrapbox.io/${process.env.PROJECT_NAME}/aiueo?body=aiueo`
-    );
-
-    const pageEditMenuSelector = "#page-edit-menu";
-    await page.waitForSelector(pageEditMenuSelector);
-    await page.click(pageEditMenuSelector);
-    console.info(`Page edit menu is Clicked.`);
-
-    const deleteMenuBtnSelector =
+    const editMenuSelector = "#page-edit-menu";
+    const deleteBtnSelector =
       '#app-container div.dropdown.open ul > li > a[title="Delete"]';
 
-    page.on("dialog", async (dialog) => {
-      await dialog.accept();
-      console.info(`Delete dialog: OK is Clicked.`);
-    });
+    // ----------------------------
+    // 以下、ページ件数分のループ処理
+    // ----------------------------
 
-    await page.waitForSelector(deleteMenuBtnSelector);
-    await page.click(deleteMenuBtnSelector);
-
-    await page.waitForTimeout(3000);
+    // UserCSS
+    const cssTagName = "#UserCSS";
+    for (let userCssPageDic of userCssPageDicList) {
+      // ページ削除
+      let cssPageUrl =
+        `https://scrapbox.io/${process.env.PROJECT_NAME}/` +
+        encodeURIComponent(userCssPageDic.title);
+      await deletePage(page, cssPageUrl, editMenuSelector, deleteBtnSelector);
+      // ページ作成
+      const cssPageEyeCatch = `[${process.env.USER_CSS_EYECATCH_URL}]`;
+      const cssPageData = `\n${cssTagName}\n\n${cssPageEyeCatch}\n\ncode:style.css\n${userCssPageDic.code}\n\n`;
+      cssPageUrl = `${cssPageUrl}?body=` + encodeURIComponent(cssPageData);
+      await addPage(page, cssPageUrl, editMenuSelector);
+    }
+    // UserScript
+    const scriptTagName = "#UserScript";
+    for (let userScriptPageDic of userScriptPageDicList) {
+      // ページ削除
+      let scriptPageUrl =
+        `https://scrapbox.io/${process.env.PROJECT_NAME}/` +
+        encodeURIComponent(userScriptPageDic.title);
+      await deletePage(
+        page,
+        scriptPageUrl,
+        editMenuSelector,
+        deleteBtnSelector
+      );
+      // ページ作成
+      const scriptPageEyeCatch = `[${process.env.USER_SCRIPT_EYECATCH_URL}]`;
+      const scriptPageData = `\n${scriptTagName}\n\n${scriptPageEyeCatch}\n\ncode:script.js\n${userScriptPageDic.code}\n\n`;
+      scriptPageUrl =
+        `${scriptPageUrl}?body=` + encodeURIComponent(scriptPageData);
+      await addPage(page, scriptPageUrl, editMenuSelector);
+    }
   } catch (error) {
     console.error(error);
     return fatalResponse();
