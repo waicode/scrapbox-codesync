9cabfcf6eaf3e4c9b8f11c7dbdbdc8e80813ffd5
waicode <waicode37@gmail.com>
2021-04-10 12:55:15 +0900

    test: push test

--- code/js/もっと太字のポップメニュー/script.js
+++ code/js/もっと太字のポップメニュー/script.js
@@ -10,3 +10,7 @@ scrapbox.PopupMenu.addButton({
   title: "****",
   onClick: (text) => `[**** ${text}]`,
 });
+scrapbox.PopupMenu.addButton({
+  title: "*****",
+  onClick: (text) => `[***** ${text}]`,
+});
